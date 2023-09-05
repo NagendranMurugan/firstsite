cf149ce702de77fba7936b96a425246fdc514aa5
NagendranMurugan <122153142+NagendranMurugan@users.noreply.github.com>
2023-09-05 19:21:35 +0530

    Add files via upload

---
+++ src/Privacy.tsx
@@ -0,0 +1,115 @@
+import type { Component } from 'solid-js';
+import Lastlogo from './assets/Lastlogo.png';
+
+const Privacy: Component = () => {
+  return (
+    <div class='font-Rubik p-4'>
+      <div class='bg-gray-300'>
+        <div class='justify-center'>
+          <div class='text-[#000000] flex text-6xl font-bold'><p>Privacy </p> <p class='text-[#7634BF]'>policy</p></div>
+        </div>
+        <br />
+        <br />
+        <div>
+          <div class=''>
+            <div class='text-[#000000] flex text-base font-bold'>Bookmyplots website is owned by </div>
+            <div class='text-[#7634BF] flex text-base font-bold'><p>Anithra Solutions</p><p class=' text-[#000000]'>, which is a data controller of your personal data.</p></div>
+          </div>
+          <br />
+          <div>
+            <div class='flex text-[#000000] text-base font-light'><p>We have adopted this Privacy Policy, which determines how we are processing the information <br />
+              collected by Feyer, which also provides the reasons why we must collect certain personal data of <br />
+              you. Therefore, you must read this Privacy Policy before using Feyer website. <br />
+              We take care of your personal data and undertake to guarantee its confidentiality and security. <br />
+              PERSONAL INFORMATION WE COLLECT:</p></div>
+          </div>
+          <br />
+          <div>
+            <div class='flex text-[#000000] text-base font-light'><p>When you visit the Feyer, we automatically collect certain information about your device, including <br />
+              information about your web browser, IP address, time zone, and some of the installed cookies on <br />
+              your device. Additionally, as you browse the Site, we collect information about the individual web <br />
+              pages or products you view, what websites or search terms referred you to the Site, and how you <br />
+              interact with the Site. We refer to this automatically-collected information as “Device Information.” <br />
+              Moreover, we might collect the personal date you provide to us (including, but not limited to <br />
+              Name, Surname, Address, payment information, etc.) during registration to be able to fulfill the <br />
+              agreement. <br /></p>
+            </div>
+          </div>
+        </div>
+        <br />
+        <div>
+          <div class=' text-[#000000] flex text-base font-bold'><p>Why do we process </p><p class=' text-[#7634BF]'>your data</p><p class='text-[#000000]'>? </p></div>
+        </div>
+        <br />
+        <div>
+          <div class='flex text-[#000000] text-base font-light'><p>Our top priority is customer data security and, as such, we may process only minimal user data, <br />
+            only as much as it is absolutely necessary to maintain the website. Information collected <br />
+            automatically is used only to identify potential cases of abuse and establish statistical information <br />
+            regarding website usage. This statistical information is not otherwise aggregated in such a way <br />
+            that it would identify any particular user of the system.</p></div>
+        </div>
+        <br />
+        <div>
+          <div class='flex text-[#000000] text-base font-light'><p>You can visit the website without telling us who you are or revealing any information, by which <br />
+            someone could identify you as a specific, identifiable individual. If, however, you wish to use some <br />
+            of the website’s features, or you wish to receive our newsletter or provide other details by filling a <br />
+            form, you may provide personal data to us, such as your email, name, last name, city of residence, <br />
+            organization, telephone number. You can choose not to provide us with your personal data, but <br />
+            then you may not be able to take advantage of some of the website’s features, for example, you <br />
+            won’t be able to receive our Newsletter or contact us directly from the website. Users who are <br />
+            uncertain about what information is mandatory are welcome to contact us via </p></div>
+        </div>
+        <div>
+          <h1 class='font-italic;'>yourwebsitename@name.com.</h1>
+        </div>
+        <br />
+        <br />
+        <div>
+          <button class='bg-[#7634BF] text-base text-white w-[190.47px] rounded-3xl font-normal my-6 mx-auto md:mx-0 py-3'>Go back to home</button>
+        </div>
+        <br />
+      </div>
+
+      <div class='bg-[#FFFFFF] p-4 '>
+        <div class='flex flex-col justify-center'>
+
+          <div class='lg:flex justify-around items-center p-5'>
+            <div class=''>
+              <div class='flex mb-10'>
+                <div class='text-2xl text-[#263238] font-bold '>
+                  <img class='' src={Lastlogo} alt='/' />
+                </div>
+              </div>
+              <h1 class='text-[#000000] text-xl font-normal mb-4'>Manage your Plots easy.</h1>
+              <p class=' text-[#000000] text-base font-light'>Privacy policy</p>
+              <h6 class='text-[#000000] text-base font-light'>Terms & Conditions</h6>
+              <div class='flex justify-evenly mt-3'>
+                <div class=' flex w-10'>
+                  <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="facebook"><path d="M15.12,5.32H17V2.14A26.11,26.11,0,0,0,14.26,2C11.54,2,9.68,3.66,9.68,6.7V9.32H6.61v3.56H9.68V22h3.68V12.88h3.06l.46-3.56H13.36V7.05C13.36,6,13.64,5.32,15.12,5.32Z"></path></svg>
+                </div>
+                <div class=' flex w-10'>
+                  <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="instagram"><path d="M17.34,5.46h0a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,17.34,5.46Zm4.6,2.42a7.59,7.59,0,0,0-.46-2.43,4.94,4.94,0,0,0-1.16-1.77,4.7,4.7,0,0,0-1.77-1.15,7.3,7.3,0,0,0-2.43-.47C15.06,2,14.72,2,12,2s-3.06,0-4.12.06a7.3,7.3,0,0,0-2.43.47A4.78,4.78,0,0,0,3.68,3.68,4.7,4.7,0,0,0,2.53,5.45a7.3,7.3,0,0,0-.47,2.43C2,8.94,2,9.28,2,12s0,3.06.06,4.12a7.3,7.3,0,0,0,.47,2.43,4.7,4.7,0,0,0,1.15,1.77,4.78,4.78,0,0,0,1.77,1.15,7.3,7.3,0,0,0,2.43.47C8.94,22,9.28,22,12,22s3.06,0,4.12-.06a7.3,7.3,0,0,0,2.43-.47,4.7,4.7,0,0,0,1.77-1.15,4.85,4.85,0,0,0,1.16-1.77,7.59,7.59,0,0,0,.46-2.43c0-1.06.06-1.4.06-4.12S22,8.94,21.94,7.88ZM20.14,16a5.61,5.61,0,0,1-.34,1.86,3.06,3.06,0,0,1-.75,1.15,3.19,3.19,0,0,1-1.15.75,5.61,5.61,0,0,1-1.86.34c-1,.05-1.37.06-4,.06s-3,0-4-.06A5.73,5.73,0,0,1,6.1,19.8,3.27,3.27,0,0,1,5,19.05a3,3,0,0,1-.74-1.15A5.54,5.54,0,0,1,3.86,16c0-1-.06-1.37-.06-4s0-3,.06-4A5.54,5.54,0,0,1,4.21,6.1,3,3,0,0,1,5,5,3.14,3.14,0,0,1,6.1,4.2,5.73,5.73,0,0,1,8,3.86c1,0,1.37-.06,4-.06s3,0,4,.06a5.61,5.61,0,0,1,1.86.34A3.06,3.06,0,0,1,19.05,5,3.06,3.06,0,0,1,19.8,6.1,5.61,5.61,0,0,1,20.14,8c.05,1,.06,1.37.06,4S20.19,15,20.14,16ZM12,6.87A5.13,5.13,0,1,0,17.14,12,5.12,5.12,0,0,0,12,6.87Zm0,8.46A3.33,3.33,0,1,1,15.33,12,3.33,3.33,0,0,1,12,15.33Z"></path></svg>
+                </div>
+                <div class=' flex w-10'>
+                  <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="twitter"><path d="M22,5.8a8.49,8.49,0,0,1-2.36.64,4.13,4.13,0,0,0,1.81-2.27,8.21,8.21,0,0,1-2.61,1,4.1,4.1,0,0,0-7,3.74A11.64,11.64,0,0,1,3.39,4.62a4.16,4.16,0,0,0-.55,2.07A4.09,4.09,0,0,0,4.66,10.1,4.05,4.05,0,0,1,2.8,9.59v.05a4.1,4.1,0,0,0,3.3,4A3.93,3.93,0,0,1,5,13.81a4.9,4.9,0,0,1-.77-.07,4.11,4.11,0,0,0,3.83,2.84A8.22,8.22,0,0,1,3,18.34a7.93,7.93,0,0,1-1-.06,11.57,11.57,0,0,0,6.29,1.85A11.59,11.59,0,0,0,20,8.45c0-.17,0-.35,0-.53A8.43,8.43,0,0,0,22,5.8Z"></path></svg>
+                </div>
+              </div>
+            </div>
+            <div class='flex  flex-col justify-center'>
+              <h1 class='text-[#000000] text-xl mb-3 mt-5  font-bold'>Subscribe to our newsletter</h1>
+              <div class="mb-4">
+                <label class="block text-gray-800 text-base font-light mb-2" for="Email address">
+                  Email address
+                </label>
+                <input class="shadow appearance-none border rounded w-[700px] py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Your email address" type="text-sm font-light" placeholder="Your email address" />
+              </div>
+              <button class='bg-[#7634BF] text-base text-white w-[133.47px] rounded-full   font-normal my-6 mx-auto md:mx-0 py-4'>Submit</button>
+            </div>
+          </div>
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Privacy;
---
+++ src/Terms.tsx
@@ -0,0 +1,120 @@
+import type { Component } from 'solid-js';
+import Lastlogo from './assets/Lastlogo.png';
+
+const Terms: Component = () => {
+    return (
+        <div>
+            <div class='font-Rubik p-4 bg-gray-300'>
+                <div>
+                    <div class='text-[#000000] flex text-6xl font-bold'><p>Terms&</p> <p class='text-[#7634BF]'>conditions</p></div>
+                </div>
+                <br />
+                <br />
+                <div>
+                    <div class='text-[#000000] flex text-base font-bold'>Welcome!</div>
+                </div>
+                <br />
+                <div>
+                    <div class='flex text-[#000000] text-base font-light'><p>These terms and conditions outline the rules and regulations for the use of BookMyPlots' <br />
+                        Website, located at example-website.com. By accessing this website, we assume you accept <br />
+                        these terms and conditions. Do not continue to use BookMyPlots if you do not agree to take all <br />
+                        of the terms and conditions stated on this page.</p></div>
+                </div>
+                <br />
+                <div>
+                    <div class='text-[#000000] flex text-base font-bold'>Cookies</div>
+                </div>
+                <br />
+                <div>
+                    <div class='flex text-[#000000] text-base font-light'><p>The website uses cookies to help personalize your online experience. By accessing BookMyPlots, <br />
+                        you agreed to use the required cookies. A cookie is a text file that is placed on your hard disk by a <br />
+                        web page server. Cookies cannot be used to run programs or deliver viruses to your computer. <br />
+                        Cookies are uniquely assigned to you and can only be read by a web server in the domain that <br />
+                        issued the cookie to you. We may use cookies to collect, store, and track information for statistical <br />
+                        or marketing purposes to operate our website. You have the ability to accept or decline optional  <br />
+                        Cookies. There are some required Cookies that are necessary for the operation of our website. <br />
+                        These cookies do not require your consent as they always work. Please keep in mind that by  <br />
+                        accepting required Cookies, you also accept third-party Cookies, which might be used via third <br />
+                        -party provided services if you use such services on our website, for example, a video display  <br />
+                        window provided by third parties and integrated into our website.</p></div>
+                </div>
+                <br />
+                <div>
+                    <div class='text-[#000000] flex text-base font-bold'>License</div>
+                </div>
+                <br />
+                <div>
+                    <div class='flex text-[#000000] text-base font-light'><p>Unless otherwise stated, Bookmyplots and <br />
+                        or its licensors own the intellectual property rights for all <br />
+                        material on Bookmyplots. All intellectual property rights are reserved. You may access this from  <br />
+                        Bookmyplots for <br />
+                        your own personal use subjected to restrictions set in these terms and conditions.</p></div>
+                </div>
+                <br />
+                <div>
+                    <div class='text-[#000000] flex text-base font-bold'>You must not</div>
+                </div>
+                <br />
+                <div>
+                    <div class='flex text-[#000000] text-base font-light'><p>Copy or republish material from Bookmyplots Sell, rent, or sub-license material from Bookmyplots. <br />
+                        duplicate or copy material from Bookmyplots. Redistribute content from Bookmyplots.
+                        <br />
+                        <br />
+                        Parts of this website offer users an opportunity to post and exchange opinions and information in <br />
+                        certain areas of the website. BookMyPlots does not filter, edit, publish, or review Comments before <br />
+                        their presence on the website. Comments do not reflect the views and opinions of BookMyPlots, <br />
+                        its agents, and/or affiliates. Comments reflect the views and opinions of the person who posts <br />
+                        their views and opinions. To the extent permitted by applicable laws, BookMyPlots shall not be <br />
+                        liable for the Comments or any liability, damages, or expenses caused and/or suffered as a result <br />
+                        of any use of and/or posting of and/or appearance of the Comments on this website.</p></div>
+                </div>
+                <br />
+        <br />
+        <div>
+          <button class='bg-[#7634BF] text-base text-white w-[190.47px] rounded-3xl font-normal my-6 mx-auto md:mx-0 py-3'>Go back to home</button>
+        </div>
+        <br />
+            </div>
+            <div class='bg-[#FFFFFF] p-4 '>
+        <div class='flex flex-col justify-center'>
+
+          <div class='lg:flex justify-around items-center p-5'>
+            <div class=''>
+              <div class='flex mb-10'>
+                <div class='text-2xl text-[#263238] font-bold '>
+                  <img class='' src={Lastlogo} alt='/' />
+                </div>
+              </div>
+              <h1 class='text-[#000000] text-xl font-normal mb-4'>Manage your Plots easy.</h1>
+              <p class=' text-[#000000] text-base font-light'>Privacy policy</p>
+              <h6 class='text-[#000000] text-base font-light'>Terms & Conditions</h6>
+              <div class='flex justify-evenly mt-3'>
+                <div class=' flex w-10'>
+                  <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="facebook"><path d="M15.12,5.32H17V2.14A26.11,26.11,0,0,0,14.26,2C11.54,2,9.68,3.66,9.68,6.7V9.32H6.61v3.56H9.68V22h3.68V12.88h3.06l.46-3.56H13.36V7.05C13.36,6,13.64,5.32,15.12,5.32Z"></path></svg>
+                </div>
+                <div class=' flex w-10'>
+                  <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="instagram"><path d="M17.34,5.46h0a1.2,1.2,0,1,0,1.2,1.2A1.2,1.2,0,0,0,17.34,5.46Zm4.6,2.42a7.59,7.59,0,0,0-.46-2.43,4.94,4.94,0,0,0-1.16-1.77,4.7,4.7,0,0,0-1.77-1.15,7.3,7.3,0,0,0-2.43-.47C15.06,2,14.72,2,12,2s-3.06,0-4.12.06a7.3,7.3,0,0,0-2.43.47A4.78,4.78,0,0,0,3.68,3.68,4.7,4.7,0,0,0,2.53,5.45a7.3,7.3,0,0,0-.47,2.43C2,8.94,2,9.28,2,12s0,3.06.06,4.12a7.3,7.3,0,0,0,.47,2.43,4.7,4.7,0,0,0,1.15,1.77,4.78,4.78,0,0,0,1.77,1.15,7.3,7.3,0,0,0,2.43.47C8.94,22,9.28,22,12,22s3.06,0,4.12-.06a7.3,7.3,0,0,0,2.43-.47,4.7,4.7,0,0,0,1.77-1.15,4.85,4.85,0,0,0,1.16-1.77,7.59,7.59,0,0,0,.46-2.43c0-1.06.06-1.4.06-4.12S22,8.94,21.94,7.88ZM20.14,16a5.61,5.61,0,0,1-.34,1.86,3.06,3.06,0,0,1-.75,1.15,3.19,3.19,0,0,1-1.15.75,5.61,5.61,0,0,1-1.86.34c-1,.05-1.37.06-4,.06s-3,0-4-.06A5.73,5.73,0,0,1,6.1,19.8,3.27,3.27,0,0,1,5,19.05a3,3,0,0,1-.74-1.15A5.54,5.54,0,0,1,3.86,16c0-1-.06-1.37-.06-4s0-3,.06-4A5.54,5.54,0,0,1,4.21,6.1,3,3,0,0,1,5,5,3.14,3.14,0,0,1,6.1,4.2,5.73,5.73,0,0,1,8,3.86c1,0,1.37-.06,4-.06s3,0,4,.06a5.61,5.61,0,0,1,1.86.34A3.06,3.06,0,0,1,19.05,5,3.06,3.06,0,0,1,19.8,6.1,5.61,5.61,0,0,1,20.14,8c.05,1,.06,1.37.06,4S20.19,15,20.14,16ZM12,6.87A5.13,5.13,0,1,0,17.14,12,5.12,5.12,0,0,0,12,6.87Zm0,8.46A3.33,3.33,0,1,1,15.33,12,3.33,3.33,0,0,1,12,15.33Z"></path></svg>
+                </div>
+                <div class=' flex w-10'>
+                  <svg xmlns="http://www.w3.org/2000/svg" data-name="Layer 1" viewBox="0 0 24 24" id="twitter"><path d="M22,5.8a8.49,8.49,0,0,1-2.36.64,4.13,4.13,0,0,0,1.81-2.27,8.21,8.21,0,0,1-2.61,1,4.1,4.1,0,0,0-7,3.74A11.64,11.64,0,0,1,3.39,4.62a4.16,4.16,0,0,0-.55,2.07A4.09,4.09,0,0,0,4.66,10.1,4.05,4.05,0,0,1,2.8,9.59v.05a4.1,4.1,0,0,0,3.3,4A3.93,3.93,0,0,1,5,13.81a4.9,4.9,0,0,1-.77-.07,4.11,4.11,0,0,0,3.83,2.84A8.22,8.22,0,0,1,3,18.34a7.93,7.93,0,0,1-1-.06,11.57,11.57,0,0,0,6.29,1.85A11.59,11.59,0,0,0,20,8.45c0-.17,0-.35,0-.53A8.43,8.43,0,0,0,22,5.8Z"></path></svg>
+                </div>
+              </div>
+            </div>
+            <div class='flex  flex-col justify-center'>
+              <h1 class='text-[#000000] text-xl mb-3 mt-5  font-bold'>Subscribe to our newsletter</h1>
+              <div class="mb-4">
+                <label class="block text-gray-800 text-base font-light mb-2" for="Email address">
+                  Email address
+                </label>
+                <input class="shadow appearance-none border rounded w-[700px] py-2 px-2 text-gray-700 leading-tight focus:outline-none focus:shadow-outline" id="Your email address" type="text-sm font-light" placeholder="Your email address" />
+              </div>
+              <button class='bg-[#7634BF] text-base text-white w-[133.47px] rounded-full   font-normal my-6 mx-auto md:mx-0 py-4'>Submit</button>
+            </div>
+          </div>
+        </div>
+        </div>
+        </div>
+    )
+}
+
+export default Terms;
